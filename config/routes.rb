e56b283f0c70488d107e0d3bab749b6b1484f615
Andre Menezes <djdedemenezes@gmail.com>
2024-05-29 11:13:28 -0300

    add some comments into routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  resources :restaurants do
-    # If inside the block, this means that it is a NESTED resource
-    # Which means:
-    # It starts with parent_resources/:parent_resources_id/the-normal-route
-    # /restaurants/:restaurant_id/reviews
-    resources :reviews, only: [:new, :create]
 
+  resources :restaurants do
     # /restaurants/whatever-you-write-inside-the-collection-block
     collection do
       # verb path
@@ -24,7 +13,13 @@ Rails.application.routes.draw do
     member do
       get :chef
     end
-    # /restaurants/42/chef
+
+    # If inside the block, this means that it is a NESTED resource
+    # Which means:
+    # It starts with parent_resources/:parent_resources_id/the-normal-route
+    # /restaurants/:restaurant_id/reviews/new
+    # /restaurants/:restaurant_id/reviews
+    resources :reviews, only: [:new, :create]
   end
 
   resources :reviews, only: :destroy
